eec4e5599bcfde0c30b561dc645c9697816dd364
amatasa <amatasa@users.noreply.github.com>
2018-01-19 14:56:01 -0500

    Add files via upload

--- home/login.php
+++ home/login.php
@@ -5,7 +5,7 @@
   }
 
   if(isset($_POST['password'])) {
-    $pass = filter_var($_POST['password'], FLITER_SANITIZE_STRING);
+    $pass = md5(filter_var($_POST['password'], FLITER_SANITIZE_STRING));
   }
 
   $conn = new mysqli("localhost", "webalex", "vr8aTp573L", "webalex_project_one");
@@ -13,13 +13,7 @@
   if($conn->connect_error) {
     echo "BIG problem";
   } else {
-    $sql = "SELECT ID, firstName, lastName, FROM Users where email='" . $email . "' and password='" . $pass . "'";
-    $result = $conn->query($sql);
-
-    if($result->num_rows > 0) {
-      $row = $result->fetch_assoc();
-      $id = $row["ID"];
-    }
-
+    $sql = "CALL webalex_project_one.GetID($email, $password)";
+    $id = $conn->query($sql);
   }
 ?>
