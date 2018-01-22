e1526e34c22446f187427f8224510b8fb96833b3
EloyGonzalez97 <eloygonzalez07@gmail.com>
2018-01-22 16:37:00 -0500

    Add files via upload

--- home/homepage.js
+++ home/homepage.js
@@ -7,6 +7,7 @@ var extension = "php";
 
 function loginUser() 
 {
+    console.log("here");
   var username = document.getElementById("uname").value;
   var password = document.getElementById("psw").value;
 
