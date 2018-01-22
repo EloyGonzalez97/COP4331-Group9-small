2e7b308a7379f315d0af18f7fc4e027ff67fc665
amatasa <amatasa@users.noreply.github.com>
2018-01-22 18:49:56 -0500

    Add files via upload

--- login.php
+++ login.php
@@ -1,27 +1,56 @@
 <?php
 
-  if(isset($_POST['email'])) {
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_EMAIL);
-  }
+  $data = getRequestInfo();
 
-  if(isset($_POST['password'])) {
-    $pass = md5(filter_var($_POST['password'], FLITER_SANITIZE_STRING));
-  }
+  $id = 0;
+  $firstName = "";
+  $lastName = "";
 
   $conn = new mysqli("localhost", "webalex", "vr8aTp573L", "webalex_project_one");
 
   if($conn->connect_error) {
+
     echo "BIG problem";
+
   } else {
-    $sql = "CALL webalex_project_one.GetID($email, $pass)";
+
+    //$sql = "CALL webalex_project_one.GetID($email, $pass)";
+    $temp = md5($data["password"]);
+    $sql = "SELECT User_ID, U_FirstName, U_LastName FROM Users where U_Email='" . $data["login"] . "' and U_Password = '" . $temp . "'";
     $result = $conn->query($sql);
 
     if($result->num_rows > 0) {
+
       $row = $result->fetch_assoc();
       $id = $row["User_ID"];
-    } else {
-      $id = 0;
-    }
+      $firstName = $row["U_FirstName"];
+      $lastName = $row["U_LastName"];
 
+    } else {
+      echo "failed query";
+      returnWithError("User does not exist");
+    }
+    $conn->close();
+  }
+
+  returnWithInfo($id, $firstName, $lastName);
+
+  function getRequestInfo(){
+    return json_decode(file_get_contents('php://input'), true);
+  }
+
+  function sendAsJson($obj) {
+    header('Content-type: application/json');
+    echo $obj;
+  }
+
+  function returnWithError($error) {
+    $ret = '{"id":0,"firstName":"","lastName":"","error":"' . $error . '"}';
+    sendAsJson($ret);
+  }
+
+  function returnWithInfo($id, $firstName, $lastName) {
+    $ret = '{"id":"' . $id . '","firstName":"' . $firstName . '","lastName":"' . $lastName . '","error":""}';
+    sendAsJson($ret);
   }
 ?>
