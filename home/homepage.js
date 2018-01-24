85bc04c834206a66451d27756c4e82f824e626cd
amatasa <amatasa@users.noreply.github.com>
2018-01-24 12:53:59 -0500

    Add files via upload

--- home/homepage.js
+++ home/homepage.js
@@ -1,4 +1,4 @@
-/*Author: ELoy Gonzalez & Alexander Alvarez */
+/*Author: ELoy Gonzalez */
 var modal = document.getElementById('id01');
 
 var urlBase = 'http://COP4331.hosted.nfoservers.com';
@@ -12,32 +12,26 @@ function loginUser()
 
   //document.getElementById("loginResult").innerHTML = "";
 	
-	var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
+	var jsonPayload = '{"login" : "' + username + '", "password" : "' + password + '"}';
 	var url = urlBase + '/login.' + extension;
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
-    
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-        
 		xhr.send(jsonPayload);
-		var jsonObject = JSON.parse(xhr.responseText);
-        
+		var jsonObject = JSON.parse( xhr.responseText );
 		
-		var userId = jsonObject.id;
-        console.log(jsonObject.firstName);
-		
-		if( userId === 0 )
+		if( userId < 1 )
 		{
-			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+            
+			//document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
 			return;
         }		
-        else
-        {
-        console.log("logged in");
-        window.location.href = 'mainpage.html';
+        
+        else{
+            Location.replace("mainpage.html")
         }
     }
 	catch(err)
@@ -48,47 +42,7 @@ function loginUser()
 
 function signUp() 
 {
-  var firstName = document.getElementById("firstname").value;
-  var lastName = document.getElementById("lastname").value;
-  var email = document.getElementById("email").value;
-  var password = document.getElementById("password").value;
-  
-
-
-  var jsonPayload = JSON.stringify ({
-	  firstName: firstName,
-	  lastName: lastName,
-	  email: email,
-	  password: password
-	 
-  })
-	var url = urlBase + '/create.' + extension;
-
-	var xhr = new XMLHttpRequest();
-	xhr.open("POST", url, false);
-	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
-	try
-	{
-		xhr.send(jsonPayload);
-		
-		var jsonObject = JSON.parse( xhr.responseText );
-		
-		userId = jsonObject.id;
-		
-		if( userId == 0 )
-		{
-			document.getElementById('errorDiv').textContent = "Error in signing up";
-			document.getElementById('errorDiv').style.visibility = "visible";
-			return;
-        }		
-        
-        else
-        window.location.href = 'mainpage.html';
-    }
-	catch(err)
-	{
-		//
-	}
+    
 }
 
 function logOut() 
