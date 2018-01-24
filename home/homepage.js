bfd55b2043b91fd636df9348fd32baa46540b1ca
Alexander Alvarez <alexander.gabriel.alvarez@Gmail.com>
2018-01-24 13:12:49 -0500

    Add files via upload

--- home/homepage.js
+++ home/homepage.js
@@ -1,4 +1,4 @@
-/*Author: ELoy Gonzalez */
+/*Author: ELoy Gonzalez & Alexander Alvarez */
 var modal = document.getElementById('id01');
 
 var urlBase = 'http://COP4331.hosted.nfoservers.com';
@@ -12,37 +12,88 @@ function loginUser()
 
   //document.getElementById("loginResult").innerHTML = "";
 	
-	var jsonPayload = '{"login" : "' + username + '", "password" : "' + password + '"}';
+	var jsonPayload = '{"username" : "' + username + '", "password" : "' + password + '"}';
 	var url = urlBase + '/login.' + extension;
 	
 	var xhr = new XMLHttpRequest();
 	xhr.open("POST", url, false);
+    
 	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
 	try
 	{
-		xhr.send(jsonPayload);
-		var jsonObject = JSON.parse( xhr.responseText );
-		
-		if( userId < 1 )
-		{
-            
-			//document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
-			return;
-        }		
         
-        else{
-            Location.replace("mainpage.html")
+		xhr.send(jsonPayload);
+		var jsonObject = JSON.parse(xhr.responseText);
+        
+		
+		var userId = jsonObject.id;
+        console.log(jsonObject.firstName);
+		
+		if( userId === 0 )
+		{
+			document.getElementById("loginResult").innerHTML = "User/Password combination incorrect";
+			return;
+			
+        }		
+        else
+        {
+        console.log("logged in");
+        window.location.href = 'mainpage.html';
         }
     }
 	catch(err)
 	{
 		//document.getElementById("loginResult").innerHTML = err.message;
 	}
+
+	return false;
 }
 
 function signUp() 
 {
-    
+  var firstName = document.getElementById("firstname").value;
+  var lastName = document.getElementById("lastname").value;
+  var email = document.getElementById("email").value;
+  var password = document.getElementById("password").value;
+  
+
+  /*
+  var jsonPayload = JSON.stringify ({
+	  firstName: firstName,
+	  lastName: lastName,
+	  email: email,
+	  password: password
+	 
+  });
+  */
+
+  	var jsonPayload = '{"firstName" : "' + firstName + '", "lastName" : "' + lastName + '", "email" : "' + email + '", "password" : "' + password + '"}';
+	var url = urlBase + '/create.' + extension;
+
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+	try
+	{
+		xhr.send(jsonPayload);
+		
+		var jsonObject = JSON.parse( xhr.responseText );
+		
+		userId = jsonObject.id;
+		
+		if( userId == 0 )
+		{
+			document.getElementById("signUpResult").innerHTML = "Error in Sign Up";
+			return;
+        }		
+        
+        else
+        window.location.href = 'mainpage.html';
+    }
+	catch(err)
+	{
+		//
+	}
 }
 
 function logOut() 
